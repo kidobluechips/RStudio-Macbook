df84c2518ebfcd0b4713489ca9febd76676de742
kidobluechips <jyu.michael@gmail.com>
2020-03-09 12:32:45 -0400

    Initial commit. Happy pulling.

---
+++ Coursera Notes.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+
+#March 9----
+browseVignettes("ggplot2")
+#How to create PDFs/Powerpoints directly from R code?
+#----
+
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
