5c4b12fd442486bc50444af35184d52014efdca6
kidobluechips <jyu.michael@gmail.com>
2020-03-09 14:10:29 -0400

    Incoming.

--- Coursera Notes.R
+++ Coursera Notes.R
@@ -3,6 +3,13 @@ library(ggplot2)
 #March 9----
 browseVignettes("ggplot2")
 #How to create PDFs/Powerpoints directly from R code?
+#R Markdown!!!
+install.packages("rmarkdown")
+
+#How do we use R to tackle big data?
+#https://rviews.rstudio.com/2019/07/17/3-big-data-strategies-for-r/
+
+#Projects not Currently Under Version Control: https://www.coursera.org/learn/data-scientists-tools/lecture/wbfrX/projects-under-version-control
 #----
 
 print("This file was created within RStudio")
